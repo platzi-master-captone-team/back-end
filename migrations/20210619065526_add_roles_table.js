7984f06e1294ffbbd1cbec04a2953f57787ee209
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-06-19 02:05:31 -0500

    [create] migrations and set up project

---
+++ migrations/20210619065526_add_roles_table.js
@@ -0,0 +1,12 @@
+exports.up = async (knex) => {
+    await knex.schema.createTable('roles', (table) => {
+        table.increments('id').primary()
+        table.string('name', 15).notNullable()
+        table.timestamp('createdAt', 10).notNullable()
+        table.timestamp('updatedAt', 10).notNullable()
+    })
+}
+
+exports.down = async (knex) => {
+    await knex.schema.dropTable('roles')
+}
---
+++ migrations/20210619065745_add_technologies_table.js
@@ -0,0 +1,14 @@
+exports.up = async (knex) => {
+    await knex.schema.createTable('technologies', (table) => {
+        table.increments('id').primary()
+        table.string('name', 45).notNullable()
+        table.integer('area_technologie_id').unsigned().notNullable()
+        table.foreign('area_technologie_id').references('id').inTable('area_technologies')
+        table.timestamp('createdAt', 10).notNullable()
+        table.timestamp('updatedAt', 10).notNullable()
+    })
+}
+
+exports.down = async (knex) => {
+    await knex.schema.dropTable('technologies')
+}
