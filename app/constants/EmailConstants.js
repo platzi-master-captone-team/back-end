1f683f731b52c9470c38ca8c7ab284fd1847b69b
Edward Fernandez <edward.fernandez@rappi.com>
2021-06-25 15:58:08 -0500

    Add function to generate emails

--- app/constants/EmailConstants.js
+++ app/constants/EmailConstants.js
@@ -1,11 +1,3 @@
-import BaseEmail from '../templates/email/BaseEmail';
-
 module.exports = {
-  EMAIL: {
-    BASE: {
-      SENDER: { name: 'John Doe', email: 'example@example.com' },
-      SUBJECT: 'Base Email',
-      CONTENT: BaseEmail,
-    },
-  },
+  BASE_EMAIL_LABEL: 'base_email',
 };
---
+++ app/routes/EmailRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const {
+  EmailToSendSchema,
+} = require('../validators/EmailSchema');
+const validate = require('../middleware/ValidationHandler');
+
+const router = express.Router();
+
+const EmailController = require('../controllers/EmailController');
+
+router.post('/', validate(EmailToSendSchema), EmailController.sendEmail);
