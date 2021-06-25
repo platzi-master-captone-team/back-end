e709dc93d417b280f3895e1b865d8e991dbbf2ee
Edward Fernandez <edward.fernandez@rappi.com>
2021-06-25 15:58:08 -0500

    Add function to generate emails

---
+++ app/services/EmailService.js
@@ -0,0 +1,41 @@
+const EmailService = module.exports;
+
+const nodemailer = require('nodemailer');
+
+const { BASE_EMAIL_LABEL } = require('../constants/EmailConstants');
+const EmailTemplateData = require('../utils/EmailTemplate');
+
+const { EMAIL_ADDRESS, EMAIL_PASSWORD } = process.env;
+
+EmailService.sendEmail = async (data) => {
+  const { email_label: emailLabel = BASE_EMAIL_LABEL, email_to: emailTo } = data;
+
+  const emailData = EmailTemplateData[emailLabel];
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: EMAIL_ADDRESS || 'ssip.team.proj01@gmail.com',
+      pass: EMAIL_PASSWORD || 'npexpxbosxflmzuh',
+    },
+  });
+
+  const mailOptions = {
+    from: EMAIL_ADDRESS,
+    to: emailTo,
+    subject: emailData.subject,
+    text: emailData.content,
+  };
+
+  try {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(`[[EmailResource.sendEmail][error] ${error}`);
+      } else {
+        console.log(`Email sent: ${info.response}`);
+      }
+    });
+  } catch (error) {
+    console.log(`[EmailResource.sendEmail][error] ${error}`);
+  }
+};
---
+++ app/utils/EmailTemplate.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "base_email": {
+        "subject": "Integration test",
+        "content": "<html><body><h1>This is an integration email</h1></body></html>",
+        "sender": "test@test.com"
+    }
+}
