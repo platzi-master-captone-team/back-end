436c0a5a0226f9cf51682877c6435a99952b0ea0
Edward Fernandez <edward.fernandez@rappi.com>
2021-07-03 21:12:39 -0400

    Added email templates for new users and bookings

--- app/utils/EmailTemplate.js
+++ app/utils/EmailTemplate.js
@@ -1,7 +1,20 @@
+const RegisterTemplate = require('../templates/RegisterTemplate');
+const BookingTemplate = require('../templates/BookingTemplate');
+
 module.exports = {
-    "base_email": {
-        "subject": "Integration test",
-        "content": "<html><body><h1>This is an integration email</h1></body></html>",
-        "sender": "test@test.com"
-    }
-}
+  base_email: {
+    subject: 'Integration test',
+    content: () => '<html><body><h1>This is an integration email</h1></body></html>',
+    sender: 'test@test.com',
+  },
+  register: {
+    subject: 'Ya eres parte de la comunidad de Consultify',
+    content: RegisterTemplate,
+    sender: 'test@test.com',
+  },
+  booking: {
+    subject: 'Ya se hizo tu reserva!',
+    content: BookingTemplate,
+    sender: 'test@test.com',
+  },
+};
